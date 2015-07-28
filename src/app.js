7fd518b4553671ad1563d5d1385d0b4b301fad53
Ire Aderinokun <ire@ireaderinokun.com>
2015-07-28 16:13:12 +0100

    Cleaning up

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@
  * DN for Pebble
  * Ire Aderinokun
  *
- * https://import.io for generating JSON Url
  */
 
+// Require dependencies
 var UI = require('ui');
 var ajax = require('ajax');
 var Vector2 = require('vector2');
 
+
+/* Loading Window */
 
 var loadingWindow = new UI.Window({
   fullscreen: true
@@ -31,25 +33,30 @@ var loadingText = new UI.Text({
   backgroundColor:'clear'
 });
 
-
 loadingWindow.add(loadingBg);
 loadingWindow.add(loadingText);
 loadingWindow.show();
 
 
+
+
+// DN Request URL
+var dnRequestURL = 'https://api.import.io/store/connector/_magic?url=https%3A%2F%2Fwww.designernews.co%2F%3Fformat%3Drss&js=false&_user=59cf7cce-4fdd-4a7f-be6f-630574c6d814&_apikey=59cf7cce4fdd4a7fbe6f630574c6d814abab1e75fbbde6cf59de38356992b106d481a5277e31e78ee98026babe24c5dfe561b854cb2bbeb5459e0aaffc4370e6b656b0ed4e47ee26c417c3672d28d410';
+
+// Function to parse the JSON object returned
 var parseFeed = function(data, quantity) {
   var items = [];
+  
   for(var i = 0; i < quantity; i++) {
     var title = data.tables[0].results[i].value_1;
     items.push({
       title: title
     });
   }
-  // Finally return whole array
+  
   return items;
 };
 
-var dnRequestURL = 'https://api.import.io/store/connector/_magic?url=https%3A%2F%2Fwww.designernews.co%2F%3Fformat%3Drss&js=false&_user=59cf7cce-4fdd-4a7f-be6f-630574c6d814&_apikey=59cf7cce4fdd4a7fbe6f630574c6d814abab1e75fbbde6cf59de38356992b106d481a5277e31e78ee98026babe24c5dfe561b854cb2bbeb5459e0aaffc4370e6b656b0ed4e47ee26c417c3672d28d410';
 
 ajax(
   {
@@ -58,10 +65,9 @@ ajax(
   },
   function(data) {
     
-    // Create an array of Menu items
+    /* Create Menu of 10 Top Stories */
     var menuItems = parseFeed(data, 10);
     
-    // Construct Menu to show to user
     var resultsMenu = new UI.Menu({
       backgroundColor: 'white',
       textColor: 'black',
@@ -72,16 +78,15 @@ ajax(
         items: menuItems
       }]
     });
-
-    // Show the Menu, hide the splash
+  
     resultsMenu.show();
     loadingWindow.hide();
-
-    // Add an action for SELECT
+    
+    
+    /* On Select of a story */
     resultsMenu.on('select', function(e) {
 
       var story = data.tables[0].results[e.itemIndex];
-
       var storyTitle = story.value_1;
       var storyDescription = story.description;
       
@@ -89,7 +94,6 @@ ajax(
         scrollable: true,
         fullscreen: true
       });
-      
 
       var headerBg = new UI.Rect({
         position: new Vector2(0, 0),
@@ -102,7 +106,6 @@ ajax(
         size: new Vector2(144, 320),
         backgroundColor: 'white'
       });
-      
       
       var Title = new UI.Text({
         position: new Vector2(8, 0),
@@ -137,9 +140,21 @@ ajax(
 
   },
   function(error) {
+    
+    var errorText = new UI.Text({
+      position: new Vector2(0, 50),
+      size: new Vector2(144, 100),
+      text:'Download failed: ' + error,
+      font:'GOTHIC_24',
+      color:'white',
+      textOverflow:'wrap',
+      textAlign:'center',
+      backgroundColor:'clear'
+    });
+    
+    loadingWindow.remove(loadingText);
+    loadingWindow.add(errorText);
+    
     console.log('Download failed: ' + error);
   }
 );
-
-
-
