1e8ca8b58a6c81b4450905d04174791defdf0476
Ire Aderinokun <ire@ireaderinokun.com>
2015-08-21 12:28:09 +0100

    Version 2.0
    Added functionality for Top Stories, Recent Stories, and Discusssions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 /**
  * DN for Pebble
  * Ire Aderinokun
+ * Version 2.0
  *
  */
 
@@ -10,85 +11,95 @@ var ajax = require('ajax');
 var Vector2 = require('vector2');
 
 
-/* Loading Window */
 
+/* Home Menu */
+var homeMenu = new UI.Menu({
+  backgroundColor: 'white',
+  textColor: 'black',
+  highlightBackgroundColor: 'blue',
+  highlightTextColor: 'white',
+  sections: [{
+    title: 'Choose a section',
+    items: [{
+      title: 'Top Stories'
+    }, {
+      title: 'Recent Stories'
+    }, {
+      title: 'Discussions'
+    }]
+  }]
+});
+homeMenu.show();
+
+
+/* Loading Window */
 var loadingWindow = new UI.Window({
   fullscreen: true
 });
-
 var loadingBg = new UI.Rect({
   position: new Vector2(0, 0),
   size: new Vector2(144, 168),
   backgroundColor: 'blue',
 });
-
 var loadingText = new UI.Text({
-  position: new Vector2(0, 50),
+  position: new Vector2(0, 60),
   size: new Vector2(144, 50),
-  text:'DN Loading',
-  font:'GOTHIC_24',
+  text:'DN for Pebble Loading',
+  font:'GOTHIC_24_BOLD',
   color:'white',
   textOverflow:'wrap',
   textAlign:'center',
   backgroundColor:'clear'
 });
-
 loadingWindow.add(loadingBg);
 loadingWindow.add(loadingText);
-loadingWindow.show();
 
 
 
+var makeRequest = function(requestURL, menuTitle) {
 
-// DN Request URL
-var dnRequestURL = 'https://api.import.io/store/connector/_magic?url=https%3A%2F%2Fwww.designernews.co%2F%3Fformat%3Drss&js=false&_user=59cf7cce-4fdd-4a7f-be6f-630574c6d814&_apikey=59cf7cce4fdd4a7fbe6f630574c6d814abab1e75fbbde6cf59de38356992b106d481a5277e31e78ee98026babe24c5dfe561b854cb2bbeb5459e0aaffc4370e6b656b0ed4e47ee26c417c3672d28d410';
-
-// Function to parse the JSON object returned
-var parseFeed = function(data, quantity) {
-  var items = [];
-  
-  for(var i = 0; i < quantity; i++) {
-    var title = data.tables[0].results[i].value_1;
-    items.push({
-      title: title
-    });
-  }
-  
-  return items;
-};
-
-
-ajax(
+  ajax(
   {
-    url: dnRequestURL,
+    url: requestURL,
     type:'json'
-  },
-  function(data) {
-    
+  }, function(data) {
+      
     /* Create Menu of 10 Top Stories */
-    var menuItems = parseFeed(data, 10);
-    
+    var menuItems = [];
+    for (var i = 0; i < 10; i++) {
+      var title = data.tables[0].results[i]['story_link/_text'];
+      menuItems.push({
+        title: title
+      });
+    }
     var resultsMenu = new UI.Menu({
       backgroundColor: 'white',
       textColor: 'black',
       highlightBackgroundColor: 'blue',
       highlightTextColor: 'white',
       sections: [{
-        title: 'Top Stories',
+        title: menuTitle,
         items: menuItems
       }]
     });
-  
     resultsMenu.show();
     loadingWindow.hide();
-    
-    
+      
+
+
+
+      
     /* On Select of a story */
     resultsMenu.on('select', function(e) {
 
       var story = data.tables[0].results[e.itemIndex];
-      var storyTitle = story.value_1;
-      var storyDescription = story.description;
+
+      var storyTitle = story['story_link/_text'];
+      var storyAuthor = story['from_link/_text'];
+      var storyTimeAgo = story['storytimeago_value'];
+      var storyCommentCount = story['commentcount_link_numbers'];
+      var storyVoteCount = story['storyvoteis_number'][0];
+
       
       var detailView = new UI.Window({
         scrollable: true,
@@ -97,22 +108,22 @@ ajax(
 
       
       var headerHeight;
-      var descriptionPos;
-      
+
       if (storyTitle.length < 40 ) {
         headerHeight = 90;
-        descriptionPos = new Vector2(8, 95);
       } else if (storyTitle.length > 39 && storyTitle.length < 50 ) {
         headerHeight = 110;
-        descriptionPos = new Vector2(8, 115);
       } else if (storyTitle.length > 49 && storyTitle.length < 60 ) {
         headerHeight = 120;
-        descriptionPos = new Vector2(8, 125);
       } else {
         headerHeight = 140;
-        descriptionPos = new Vector2(8, 145);
       }
 
+      var lineHeight = 25;
+      var timeAgoPosY = headerHeight + 5;
+      var voteCountPosY = headerHeight + lineHeight + 5;
+      var commentCountPostY = headerHeight + (lineHeight * 2) + 5;
+      var authorPosY = headerHeight + (lineHeight * 3) + 5;
 
 
       var headerBg = new UI.Rect({
@@ -134,13 +145,14 @@ ajax(
       
       var bodyBg = new UI.Rect({
         position: new Vector2(0, headerHeight),
-        size: new Vector2(144, 320),
+        size: new Vector2(144, 140),
         backgroundColor: 'white'
       });
-      var Description = new UI.Text({
-        position: descriptionPos,
-        size: new Vector2(131, 300),
-        text: storyDescription,
+
+      var TimeAgo = new UI.Text({
+        position: new Vector2(8, timeAgoPosY),
+        size: new Vector2(131, lineHeight),
+        text: 'Posted ' + storyTimeAgo,
         font:'GOTHIC_18',
         color:'black',
         textOverflow:'wrap',
@@ -148,33 +160,98 @@ ajax(
         backgroundColor:'clear'
       });
 
+      var VoteCount = new UI.Text({
+        position: new Vector2(8, voteCountPosY),
+        size: new Vector2(131, lineHeight),
+        text: storyVoteCount + ' votes',
+        font:'GOTHIC_18',
+        color:'black',
+        textOverflow:'wrap',
+        textAlign:'left',
+        backgroundColor:'clear'
+      });
 
+      var CommentCount = new UI.Text({
+        position: new Vector2(8, commentCountPostY),
+        size: new Vector2(131, lineHeight),
+        text: storyCommentCount + ' comments',
+        font:'GOTHIC_18',
+        color:'black',
+        textOverflow:'wrap',
+        textAlign:'left',
+        backgroundColor:'clear'
+      });
+
+      var Author = new UI.Text({
+        position: new Vector2(8, authorPosY),
+        size: new Vector2(131, 50),
+        text: 'Posted by ' + storyAuthor,
+        font:'GOTHIC_18',
+        color:'black',
+        textOverflow:'wrap',
+        textAlign:'left',
+        backgroundColor:'clear'
+      });
+
+      
       detailView.add(headerBg);
       detailView.add(bodyBg);
       detailView.add(Title);
-      detailView.add(Description);
+      detailView.add(TimeAgo);
+      detailView.add(CommentCount);
+      detailView.add(Author);
+      detailView.add(VoteCount);
       detailView.show();
 
-    });
+    }); // end resultsMenu.onselect
 
 
-  },
-  function(error) {
-    
-    var errorText = new UI.Text({
-      position: new Vector2(0, 50),
-      size: new Vector2(144, 100),
-      text:'Download failed: ' + error,
-      font:'GOTHIC_24',
-      color:'white',
-      textOverflow:'wrap',
-      textAlign:'center',
-      backgroundColor:'clear'
-    });
-    
-    loadingWindow.remove(loadingText);
-    loadingWindow.add(errorText);
-    
-    console.log('Download failed: ' + error);
+  }, function(error) {
+      
+      var errorText = new UI.Text({
+        position: new Vector2(0, 50),
+        size: new Vector2(144, 100),
+        text:'Download failed: ' + error,
+        font:'GOTHIC_24',
+        color:'white',
+        textOverflow:'wrap',
+        textAlign:'center',
+        backgroundColor:'clear'
+      });
+      
+      loadingWindow.remove(loadingText);
+      loadingWindow.add(errorText);
+      
+      console.log('Download failed: ' + error);
+
+  }); // end AJAX
+
+
+
+}; // end makeRequest function
+
+
+
+var topStoriesReuqestURL = 'https://api.import.io/store/connector/_magic?url=https%3A%2F%2Fwww.designernews.co%2F&js=false&_user=59cf7cce-4fdd-4a7f-be6f-630574c6d814&_apikey=59cf7cce4fdd4a7fbe6f630574c6d814abab1e75fbbde6cf59de38356992b106d481a5277e31e78ee98026babe24c5dfe561b854cb2bbeb5459e0aaffc4370e6b656b0ed4e47ee26c417c3672d28d410';
+var newStoriesReuqestURL = 'https://api.import.io/store/connector/_magic?url=https%3A%2F%2Fwww.designernews.co%2Fnew&js=false&_user=59cf7cce-4fdd-4a7f-be6f-630574c6d814&_apikey=59cf7cce4fdd4a7fbe6f630574c6d814abab1e75fbbde6cf59de38356992b106d481a5277e31e78ee98026babe24c5dfe561b854cb2bbeb5459e0aaffc4370e6b656b0ed4e47ee26c417c3672d28d410';
+var discussionsRequestURL = 'https://api.import.io/store/connector/_magic?url=https%3A%2F%2Fwww.designernews.co%2Fdiscussions&js=false&_user=59cf7cce-4fdd-4a7f-be6f-630574c6d814&_apikey=59cf7cce4fdd4a7fbe6f630574c6d814abab1e75fbbde6cf59de38356992b106d481a5277e31e78ee98026babe24c5dfe561b854cb2bbeb5459e0aaffc4370e6b656b0ed4e47ee26c417c3672d28d410';
+
+homeMenu.on('select', function(e) {
+
+  var section = e.itemIndex;
+  homeMenu.hide();
+  loadingWindow.show();
+
+  if ( section === 0 ) {
+    // Top Stories
+    makeRequest(topStoriesReuqestURL, "Top Stories");
+  } 
+  else if ( section === 1 ) {
+    // Recent Stories
+    makeRequest(newStoriesReuqestURL, "Recent Stories"); 
+  } 
+  else if ( section === 2 ) {
+    // Discussions Stories
+    makeRequest(discussionsRequestURL, "Discussions");
   }
-);
+});
